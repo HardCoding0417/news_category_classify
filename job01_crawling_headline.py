f2fda259f20b8ed9d3d3a521d3f5031e2ff1ba57
문정환 <hardluck0417@gmail.com>
2023-10-10 16:51:46 +0900

    2023-10-10일 뉴스의 헤드라인을 따는 프로그램

--- job01_crawling_headline.py
+++ job01_crawling_headline.py
@@ -14,7 +14,7 @@ re_title = re.compile('[^가-힣]|a-z|A-Z')
 for i in range(6):
     response = requests.get(https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}'.format(i),)
 
-##
+###
 
 
 # response = requests.get(url, headers=headers)
