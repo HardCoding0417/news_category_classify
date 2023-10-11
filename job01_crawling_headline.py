2a12551e72f47bf491805fac8e6f45646adef9cb
문정환 <hardluck0417@gmail.com>
2023-10-11 10:24:31 +0900

    2023-10-11일 정적 크롤러

--- job01_crawling_headline.py
+++ job01_crawling_headline.py
@@ -3,6 +3,7 @@ import requests
 import re
 import pandas as pd
 import datetime
+from time import sleep
 
 category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
 url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100'
@@ -12,16 +13,26 @@ df_titles = pd.DataFrame()
 re_title = re.compile('[^가-힣]|a-z|A-Z')
 
 for i in range(6):
-    response = requests.get(https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}'.format(i),)
+    response = requests.get('https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}'.format(i), headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title_tags = soup.select('.sh_text_headline')
+    titles = []
+    for title_tag in title_tags:
+        titles.append(re.compile('[^가-힣]|a-z|A-Z').sub(' ', title_tag.text))
+    df_section_titles = pd.DataFrame(titles, columns=['titles'])
+    df_section_titles['category'] = category[i]
+    df_titles = pd.concat([df_titles, df_section_titles], axis='rows')
 
-#####
-
+print(df_titles.head())
+df_titles.info()
+print(df_titles['category'].value_counts())
+df_titles.to_csv('./crawling_data/naver_headline_news_{}.csv'.format(
+    datetime.datetime.now().strftime('%Y%m%d')), index=False)
 
 # response = requests.get(url, headers=headers)
 # # print(list(response))
 # # print(type(response))
 
-# soup = BeautifulSoup(response.text, 'html.parser')
 # # print(soup)
 
 # title_tags = soup.select('.sh_text_headline')
@@ -41,7 +52,6 @@ for i in range(6):
 # # https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100
 # # 정치 헤드라인의 형식
 # # <a href="https://n.news.naver.com/mnews/article/032/0003253663?sid=100"
-#
 #
 # # class="sh_text_headline nclicks(cls_pol.clsart)">강서구청장 보궐선거 결과가 가져올 여권의 ‘다른 미래’</a>
 #
